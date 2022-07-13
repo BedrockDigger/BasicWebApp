d23e3bcda45652193ca229298ca3093f3b64248e
Estel <callmebedrockdigger@gmail.com>
2022-07-13 17:07:48 +0200

    nice

--- src/main/java/de/tum/in/ase/eist/QueryProcessor.java
+++ src/main/java/de/tum/in/ase/eist/QueryProcessor.java
@@ -15,6 +15,11 @@ public class QueryProcessor {
                     "writer in the English language and the world's pre-eminent dramatist.";
         } else if (query.contains("which of the following numbers is the largest")) {
             return String.valueOf(Arrays.stream(query.split(":")[1].split(",")).map(String::trim).mapToInt(Integer::parseInt).max().orElse(0));
+        }
+        if (query.contains("plus")) {
+            String[] chunks = query.split(" ");
+            int sum = Integer.parseInt(chunks[2]) + Integer.parseInt(chunks[4]);
+            return String.valueOf(sum);
         } else {
             return "";
         }
