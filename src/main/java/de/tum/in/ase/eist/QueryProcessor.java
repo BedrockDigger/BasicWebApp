844238ad781ed1bbd1ecade32fa641cb3e57ca88
Estel <callmebedrockdigger@gmail.com>
2022-07-13 16:59:09 +0200

    nice

--- src/main/java/de/tum/in/ase/eist/QueryProcessor.java
+++ src/main/java/de/tum/in/ase/eist/QueryProcessor.java
@@ -2,17 +2,19 @@ package de.tum.in.ase.eist;
 
 import org.springframework.stereotype.Service;
 
+import java.util.Arrays;
+
 @Service
 public class QueryProcessor {
 
     public String process(String query) {
-		query = query.toLowerCase();
+        query = query.toLowerCase();
         if (query.contains("shakespeare")) {
             return "William Shakespeare (26 April 1564 - 23 April 1616) was an " +
                     "English poet, playwright, and actor, widely regarded as the greatest " +
                     "writer in the English language and the world's pre-eminent dramatist.";
-        } else if (query.contains("name")) {
-           return "eist_noob";
+        } else if (query.contains("which of the following numbers is the largest")) {
+            return String.valueOf(Arrays.stream(query.split(":")[1].split(",")).map(String::trim).mapToInt(Integer::parseInt).max().orElse(0));
         } else {
             return "";
         }
